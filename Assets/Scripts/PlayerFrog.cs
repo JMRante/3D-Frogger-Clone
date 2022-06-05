a312a8c3d042874efbf165667e97b8d2fe557ead
Joshua Stark <joshuamstark@gmail.com>
2022-06-04 23:50:33 -0700

    Player frog has a raised arc to their hops. Player state is tracked for more specified behaviors such as different move timers and whether or not vertical hop should happen. Normalized move timer can now be calculated correctly as well with the state.

--- Assets/Scripts/PlayerFrog.cs
+++ Assets/Scripts/PlayerFrog.cs
@@ -2,10 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum PlayerState
+{
+    STANDING,
+    HOPPING,
+    SUPERHOPPING,
+    TURNINGRIGHT,
+    TURNINGLEFT
+}
+
 public class PlayerFrog : MonoBehaviour
 {
     public float hopTime = 0.1f;
-    private float hopTimer = 0f;
+    public float superHopTime = 0.2f;
+    public float turnTime = 0.05f;
+    private float moveTimer = 0f;
+
+    public float hopHeight = 0.5f;
+
+    private PlayerState state;
 
     private Vector3 lastPosition;
     private Vector3 nextPosition;
@@ -20,6 +35,8 @@ public class PlayerFrog : MonoBehaviour
 
         lastRotation = transform.rotation;
         nextRotation = transform.rotation;
+
+        state = PlayerState.STANDING;
     }
 
     void Update()
@@ -29,65 +46,99 @@ public class PlayerFrog : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.UpArrow))
         {
             hopDirection = Vector3.forward;
+            state = PlayerState.HOPPING;
         }
         else if (Input.GetKeyDown(KeyCode.RightArrow))
         {
             hopDirection = Vector3.right;
+            state = PlayerState.HOPPING;
         }
         else if (Input.GetKeyDown(KeyCode.DownArrow))
         {
             hopDirection = Vector3.back;
+            state = PlayerState.HOPPING;
         }
         else if (Input.GetKeyDown(KeyCode.LeftArrow))
         {
             hopDirection = Vector3.left;
-        }
-        else if (Input.GetKeyDown(KeyCode.W))
-        {
-            hopDirection = transform.forward * 2f;
-        }
-        else if (Input.GetKeyDown(KeyCode.Q))
-        {
-            if (hopTimer == 0f)
-            {
-                nextRotation = transform.rotation * Quaternion.AngleAxis(-90f, Vector3.up);
-                hopTimer = hopTime;
-            }
+            state = PlayerState.HOPPING;
         }
         else if (Input.GetKeyDown(KeyCode.E))
         {
-            if (hopTimer == 0f)
+            hopDirection = transform.forward * 2f;
+            state = PlayerState.SUPERHOPPING;
+        }
+        else if (Input.GetKeyDown(KeyCode.Q))
+        {
+            if (moveTimer == 0f)
+            {
+                nextRotation = transform.rotation * Quaternion.AngleAxis(-90f, Vector3.up);
+                moveTimer = turnTime;
+                state = PlayerState.TURNINGLEFT;
+            }
+        }
+        else if (Input.GetKeyDown(KeyCode.W))
+        {
+            if (moveTimer == 0f)
             {
                 nextRotation = transform.rotation * Quaternion.AngleAxis(90f, Vector3.up);
-                hopTimer = hopTime;
+                moveTimer = turnTime;
+                state = PlayerState.TURNINGRIGHT;
             }
         }
 
-        if (hopDirection != Vector3.zero && hopTimer == 0f)
+        if (hopDirection != Vector3.zero && moveTimer == 0f)
         {
             nextPosition = lastPosition + hopDirection;
             nextRotation = Quaternion.LookRotation(hopDirection, Vector3.up);
-            hopTimer = (lastPosition - nextPosition).magnitude * hopTime;
+
+            switch (state)
+            {
+                case PlayerState.HOPPING: moveTimer = hopTime; break;
+                case PlayerState.SUPERHOPPING: moveTimer = superHopTime; break;
+            }
         }
 
-        if (hopTimer > 0f)
+        if (moveTimer > 0f)
         {
-            hopTimer -= Time.deltaTime;
+            moveTimer -= Time.deltaTime;
 
-            float normalizedHopTimer = 1 - (hopTimer / hopTime);
-            transform.position = Vector3.Slerp(lastPosition, nextPosition, normalizedHopTimer);
-            transform.rotation = Quaternion.Slerp(lastRotation, nextRotation, normalizedHopTimer);
+            float normalizedMoveTimer = GetNormalizedMoveTimer();
+            float hopHeightCurveY = (hopHeight * 4) * (-normalizedMoveTimer * normalizedMoveTimer + normalizedMoveTimer);
+
+            transform.position = Vector3.Slerp(lastPosition, nextPosition, normalizedMoveTimer);
+            
+            if (state != PlayerState.TURNINGLEFT && state != PlayerState.TURNINGRIGHT)
+            {
+                transform.position += (Vector3.up * hopHeightCurveY);
+            }
+
+            transform.rotation = Quaternion.Slerp(lastRotation, nextRotation, normalizedMoveTimer);
         }
 
-        if (hopTimer <= 0f)
+        if (moveTimer <= 0f)
         {
-            hopTimer = 0f;
+            moveTimer = 0f;
 
             transform.position = nextPosition;
             lastPosition = transform.position;
 
             transform.rotation = nextRotation;
             lastRotation = transform.rotation;
+
+            state = PlayerState.STANDING;
+        }
+    }
+
+    private float GetNormalizedMoveTimer()
+    {
+        switch (state)
+        {
+            case PlayerState.HOPPING: return 1 - (moveTimer / hopTime);
+            case PlayerState.SUPERHOPPING: return 1 - (moveTimer / superHopTime);
+            case PlayerState.TURNINGLEFT: return 1 - (moveTimer / turnTime);
+            case PlayerState.TURNINGRIGHT: return 1 - (moveTimer / turnTime);
+            default: return 0;
         }
     }
 }
